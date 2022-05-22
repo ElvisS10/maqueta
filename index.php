91c59370eea71bdf2cf1196fd689a48cb623979f
Elvis <72048969+ElvisS10@users.noreply.github.com>
2022-05-21 20:49:14 -0500

    maqueta

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Maqueta</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/imagen.css" rel="stylesheet" />
+    <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-primary">
+    <header class="bd-header bg-success py-3 d-flex align-items-stretch border-bottom border-dark">
+        <div class="container-fluid d-flex align-items-center">
+            <h1 class="d-flex align-items-center fs-4 text-white mb-0 text-warning">
+                <img src="imagenes/ico_estrella.png" alt="50" width="50" class="me-3">
+                <b class="text-warning">¡Activa la promoción!</b>
+            </h1>
+            <a class="ms-auto link-light img-fluid" href="#">Inicio de sesion </a> <img src="imagenes/ícono_3 usuario.png" alt="50" width="50">
+        </div>
+    </header>
+
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_content">
+            <main>
+                <picture>
+                    <img src="imagenes/female_tourist.jpg" class="img-fluid img-thumbnail" alt="">
+                </picture>
+            </main>
+        </div>
+        <div class="panel">
+            <img src="imagenes/ico_llamada.png" class="call" alt="">
+            <h1 class="now" style="position: absolute; top: 190px;">¡El mejor momento es ahora!</h1>
+            <SPAN class="llamanos" style="position: absolute; top: 520px; left: 200px;">
+            ¡Llamanos!
+            
+        </div>
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <h1 align="center" class="text-light" style="position: absolute; top: 150px;" >Cantacta nuestras</h1>
+                    <h2 align="center" style="position: absolute; top: 280px;">Redes sociales</h2>
+                    <img src="imagenes/ícono_5 facebook.png" class="fb" alt="25%" width="25%">
+                    <img src="imagenes/ícono_5 instagram.png" class="ig" alt="25%" width="25%">
+                    <img src="imagenes/ícono_5 twitter.png" class="tt" alt="25%" width="25%">
+                </div>
+                <div class="sb-sidenav-footer text-light">
+                    Todos los derechos reservados &copy; 2022
+                </div>
+            </nav>
+        </div>
+    </div>
+
+  
+</body>
+        <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</html>
